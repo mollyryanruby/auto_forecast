eac23e4e787c5594a8098221798c6f78165f60e6
molly.ruby <molly.ruby@effem.com>
2023-08-02 13:13:54 -0600

    added error raising to plot_values_per_group

--- auto_forecast/plotting.py
+++ auto_forecast/plotting.py
@@ -5,13 +5,17 @@ import seaborn as sns
 # standard data manipulation imports
 import pandas as pd
 
+from statsmodels.tsa.api import graphics
+
 # intra-package code
 import parameters as p
 
-def periodic_values_hist(df, value_col='value', figsize=p.FIG_SIZE):
+def plot_periodic_values_hist(data, value_col, figsize=p.FIG_SIZE, color=p.PLOT_COLOR_1):
+    if value_col not in data.columns:
+        raise ValueError("value_col must exist within the data columns.")
 
     fig, ax = plt.subplots(figsize=figsize)
-    plt.hist(df[value_col], color='mediumblue')
+    plt.hist(data[value_col], color=color)
     
     ax.set(
         xlabel = f"{value_col} per day",
@@ -21,17 +25,21 @@ def periodic_values_hist(df, value_col='value', figsize=p.FIG_SIZE):
     return fig, ax
     
 
-def values_per_group(df, value_col, group_cols, figsize=p.FIG_SIZE, fill_color='mediumblue'):
+def plot_values_per_group(data, value_col, group_cols, figsize=p.FIG_SIZE, fill_color=p.PLOT_COLOR_1):
+    if value_col not in data.columns:
+        raise ValueError("value_col must exist within the data columns.")
+    if not isinstance(group_cols, str) and not isinstance(group_cols, list):
+        raise KeyError("group_cols must be a stirng or list.")
+    if isinstance(group_cols, str):
+        group_cals = list(group_cols)
     
-    if len(group_cols)<1:
-        raise ValueError("group_cols requires at least one column.")
-    
+    data[group_cols] = data[group_cols].astype(str)
+    data['group_id'] = data[group_cols].agg('-'.join, axis=1) if len(group_cols)>1 else data[group_cols]
 
-    df['group_id'] = df[group_cols].agg('-'.join, axis=1)
-    by_group = df.groupby('group_id')[value_col].sum().reset_index()
+    by_group = data.groupby('group_id')[value_col].sum().reset_index()
     
     fig, ax = plt.subplots(figsize=figsize)
-    sns.barplot(df['group_id'], df[value_col], color=fill_color)
+    sns.barplot(x=by_group['group_id'], y=by_group[value_col], color=fill_color)
     
     ax.set(
         xlabel = "group id",
@@ -42,25 +50,28 @@ def values_per_group(df, value_col, group_cols, figsize=p.FIG_SIZE, fill_color='
     sns.despine()
     return fig, ax
 
-def standard_time_plot(df, date_col, value_col, mean_freq, figsize=p.FIG_SIZE):
-
+def plot_time_series(data, date_col, value_col, mean_freq, figsize=p.FIG_SIZE, color=p.PLOT_COLOR_1):
+    if date_col not in data.columns:
+        raise ValueError("date_col must exist within the data columns.")
+    if value_col not in data.columns:
+        raise ValueError("value_col must exist within the data columns.")
 
     fig, ax = plt.subplots(figsize=figsize)
     sns.lineplot(
-        df[date_col], 
-        df[value_col], 
-        data=df, 
+        data[date_col], 
+        data[value_col], 
+        data=data, 
         ax=ax, 
-        color='mediumblue', 
+        color=color, 
         label='Total')
     
-    df[date_col] = pd.to_datetime(df[date_col])
-    average_df = df.groupby(pd.Grouper(key=date_col, freq=mean_freq)).mean().reset_index()
+    data[date_col] = pd.to_datetime(data[date_col])
+    average_data = data.groupby(pd.Grouper(key=date_col, freq=mean_freq)).mean().reset_index()
 
     sns.lineplot(
-        average_df[date_col], 
-        average_df[value_col], 
-        data=average_df, 
+        average_data[date_col], 
+        average_data[value_col], 
+        data=average_data, 
         ax=ax, 
         color='red', 
         label='Average'
@@ -70,4 +81,40 @@ def standard_time_plot(df, date_col, value_col, mean_freq, figsize=p.FIG_SIZE):
            ylabel = "values",
            title = "total and mean timeplot")
     
+    sns.despine()
+
+def plt_acf_pcf(data, value_col, lags=None, color='mediumblue', figsize=p.FIG_SIZE, **kwargs):
+    if value_col not in data.columns:
+        raise ValueError("value_col must exist within the data columns.")
+    
+    # Convert dataframe to datetime index
+    dt_data = data.set_index('date')[[value_col]]
+    
+    fig, ax = plt.subplots(nrows=1, ncols=3, figsize=figsize)
+    
+    dt_data.plot(ax=ax[0], color=color)
+    graphics.plot_acf(dt_data, lags=lags, ax=ax[1], color=color)
+    graphics.plot_pacf(dt_data, lags=lags, ax=ax[2], color=color)
+
+    sns.despine()
+    plt.tight_layout()
+
+def plot_results(results, original_df, model_name, figsize=p.FIG_SIZE, color1=p.PLOT_COLOR_1, color2=p.PLOT_COLOR_2):
+    """Plots predictions over original data to visualize results. Saves each
+    plot as a png.
+
+    Keyword arguments:
+    -- results: a dataframe with unscaled predictions
+    -- original_df: the original monthly sales dataframe
+    -- model_name: the name that will be used in the plot title
+    """
+    fig, ax = plt.subplots(figsize=figsize)
+    sns.lineplot(original_df.date, original_df.sales, data=original_df, ax=ax,
+                 label='Original', color=color1)
+    sns.lineplot(results.date, results.pred_value, data=results, ax=ax,
+                 label='Predicted', color=color2)
+    ax.set(xlabel="Date",
+           ylabel="Sales",
+           title=f"{model_name} Sales Forecasting Prediction")
+    ax.legend()
     sns.despine()

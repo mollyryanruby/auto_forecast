b92d5c53c5dd0d0e3fb8e036552e8e7565e539a6
molly.ruby <molly.ruby@effem.com>
2023-08-21 13:46:38 -0400

    created class SalesForecasting

--- auto_forecast/src/modeling.py
+++ auto_forecast/src/modeling.py
@@ -3,166 +3,179 @@ import numpy as np
 from sklearn.metrics import (mean_squared_error, 
                              mean_absolute_error, 
                             r2_score)
-
+from sklearn.linear_model import LinearRegression
 from src.plotting import (plot_results)
 
+def isvalid_value(value, values_list):
+    if value not in values_list:
+        raise ValueError(f"{value} not in the list of available options: {values_list}")
+    
+class SalesForecasting:
 
-def predict_df(unscaled_predictions, original_df):
-    """Generates a dataframe that shows the predicted sales for each month
-    for plotting results.
+    def __init__(self, model_list):
 
-    Keyword arguments:
-    -- unscaled_predictions: the model predictions that do not have min-max or
-                             other scaling applied
-    -- original_df: the original monthly sales dataframe
-    """
-    #create dataframe that shows the predicted sales
-    result_list = []
-    sales_dates = list(original_df[-13:].date)
-    act_sales = list(original_df[-13:].sales)
-
-    for index in range(len(unscaled_predictions)):
-        result_dict = {
-            'pred_value': int(
-                unscaled_predictions[index][0] + act_sales[index]
-            ),
-            'date': sales_dates[index + 1],
+        self.model_list_options = {
+            'LinearRegression': {
+                'model': LinearRegression(),
+                'type': 'regression'
+                }
         }
-        result_list.append(result_dict)
 
-    return pd.DataFrame(result_list)
+        if not isinstance(model_list, list):
+            raise TypeError('model_dict must be a list.')
+        if len(model_list)<1: 
+            raise ValueError("model_list must have at least one input.")
+        for model in model_list:
+            isvalid_value(model, self.model_list_options.keys())
+            
+        self.model_list = model_list
+        self.stored_models = {model_name: {} for model_name in model_list}
+    
+    def fit(self, X_train, y_train):
+        
+        model_list_options = self.model_list_options
+        self.X_train = X_train
+        self.y_train = y_train
 
-def get_scores(unscaled_df, original_df, model_name):
-    """Prints the root mean squared error, mean absolute error, and r2 scores
-    for each model. Saves all results in a model_scores dictionary for
-    comparison.
+        # train each of the models provided in model_dict 
+        for model_name in self.model_list:
+            if model_list_options[model_name]['type']=='regression':
+                self.stored_models[model_name]['model'] = self.__fit_regression_model(model_list_options[model_name]['model'])
+            else: 
+                print('Model fit not found.')
+    
+    def __fit_regression_model(self, model):
+        return model.fit(self.X_train, self.y_train)
+    
+    def predict(self, x_values, y_values=None, scaler=None, print_scores=False):
 
-    Keyword arguments:
-    -- unscaled_predictions: the model predictions that do not have min-max or
-                             other scaling applied
-    -- original_df: the original monthly sales dataframe
-    -- model_name: the name that will be used to store model scores
-    """
-    rmse = np.sqrt(mean_squared_error(original_df.sales[-12:], unscaled_df.pred_value[-12:]))
-    mae = mean_absolute_error(original_df.sales[-12:], unscaled_df.pred_value[-12:])
-    r2 = r2_score(original_df.sales[-12:], unscaled_df.pred_value[-12:])
-    model_scores[model_name] = [rmse, mae, r2]
+        model_list_options = self.model_list_options
 
-    print(f"RMSE: {rmse}")
-    print(f"MAE: {mae}")
-    print(f"R2 Score: {r2}")
+        self.y_validation_values = y_values
+        self.x_predictor_values = x_values
+        self.scaler = scaler
 
+        for model_name in self.model_list:
+            if model_list_options[model_name]['type']=='regression':
+                predictions = self.__predict_regression_model(model_list_options[model_name]['model'])
+            else: 
+                print('Model fit not found.')
 
+            # Undo scaling to compare predictions against original data
+            if scaler is not None:
+                unscale_values = np.concatenate((x_values.values, predictions), axis=1)
+                predictions = [row[-1] for row in self.__undo_scaling(unscale_values, scaler)]
 
-def regressive_model(train_data, test_data, model, model_name):
-    """Runs regressive models in SKlearn framework. First calls scale_data
-    to split into X and y and scale the data. Then fits and predicts. Finally,
-    predictions are unscaled, scores are printed, and results are plotted and
-    saved.
+            self.stored_models[model_name]['predictions'] = predictions
+            
+            if y_values is not None: 
+                unscale_values_true = np.concatenate((x_values.values, y_values), axis=1)
+                y_values = [row[-1] for row in self.__undo_scaling(unscale_values_true, scaler)]
+                self.stored_models[model_name]['true_values'] = y_values
+                _, _, _ = self.get_scores(predictions, y_values, model_name, print_scores)
+        
+        return self
+    
+    def __predict_regression_model(self, model):
+        return model.predict(self.x_predictor_values)
 
-    Keyword arguments:
-    -- train_set: dataset used to train the model
-    -- test_set: dataset used to test the model
-    -- model: the sklearn model and model arguments in the form of
-              model(kwarga)
-    -- model_name: the name that will be used to store model scores and plotting
-    """
+    def __undo_scaling(self, values, scaler):
+        return scaler.inverse(values)
+    
+    def get_scores(self, y_pred, y_true, model_name=None, print_scores=False):
+        rmse = np.sqrt(mean_squared_error(y_pred, y_true))
+        mae = mean_absolute_error(y_pred, y_true)
+        r2 = r2_score(y_pred, y_true)
 
-    # Split into X & y and scale data
-    X_train, y_train, X_test, y_test, scaler_object = scale_data(train_data,
-                                                                 test_data)
-    # Run sklearn models
-    mod = model
-    mod.fit(X_train, y_train)
-    predictions = mod.predict(X_test)
+        if model_name:
+            self.stored_models[model_name]['rmse'] = rmse
+            self.stored_models[model_name]['mae'] = mae
+            self.stored_models[model_name]['r2'] = r2
 
-    # Undo scaling to compare predictions against original data
-    original_df = load_data('../data/monthly_data.csv')
-    unscaled = undo_scaling(predictions, X_test, scaler_object)
-    unscaled_df = predict_df(unscaled, original_df)
+        if print_scores:
+            if model_name:
+                print(f"Scores for {model_name}")
+            print("\tRoot mean squared error: {rmse}")
+            print("\tMean absolute error: {mae}")
+            print("\tR Squared: {r2}")
 
-    # print scores and plot results
-    get_scores(unscaled_df, original_df, model_name)
-    plot_results(unscaled_df, original_df, model_name)
+        return rmse, mae, r2
+    
 
-def lstm_model(train_data, test_data):
-    """Runs a long-short-term-memory nueral net with 2 dense layers. Generates
-    predictions that are then unscaled. Scores are printed and results are
-    plotted and saved.
+# def lstm_model(train_data, test_data):
+#     """Runs a long-short-term-memory nueral net with 2 dense layers. Generates
+#     predictions that are then unscaled. Scores are printed and results are
+#     plotted and saved.
 
-    Keyword arguments:
-    -- train_set: dataset used to train the model
-    -- test_set: dataset used to test the model
-    """
+#     Keyword arguments:
+#     -- train_set: dataset used to train the model
+#     -- test_set: dataset used to test the model
+#     """
 
-    # Split into X & y and scale data
-    X_train, y_train, X_test, y_test, scaler_object = scale_data(train_data, test_data)
+#     # Split into X & y and scale data
+#     X_train, y_train, X_test, y_test, scaler_object = scale_data(train_data, test_data)
 
-    X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1])
-    X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1])
+#     X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1])
+#     X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1])
 
-    # Build LSTM
-    model = Sequential()
-    model.add(LSTM(4, batch_input_shape=(1, X_train.shape[1], X_train.shape[2]), stateful=True))
-    model.add(Dense(1))
-    model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(X_train, y_train, epochs=200, batch_size=1, verbose=1, shuffle=False)
-    predictions = model.predict(X_test, batch_size=1)
+#     # Build LSTM
+#     model = Sequential()
+#     model.add(LSTM(4, batch_input_shape=(1, X_train.shape[1], X_train.shape[2]), stateful=True))
+#     model.add(Dense(1))
+#     model.add(Dense(1))
+#     model.compile(loss='mean_squared_error', optimizer='adam')
+#     model.fit(X_train, y_train, epochs=200, batch_size=1, verbose=1, shuffle=False)
+#     predictions = model.predict(X_test, batch_size=1)
 
-    # Undo scaling to compare predictions against original data
-    original_df = load_data('../data/monthly_data.csv')
-    unscaled = undo_scaling(predictions, X_test, scaler_object, lstm=True)
-    unscaled_df = predict_df(unscaled, original_df)
+#     # Undo scaling to compare predictions against original data
+#     original_df = load_data('../data/monthly_data.csv')
+#     unscaled = undo_scaling(predictions, X_test, scaler_object, lstm=True)
+#     unscaled_df = predict_df(unscaled, original_df)
 
-    # print scores and plot results
-    get_scores(unscaled_df, original_df, 'LSTM')
-    plot_results(unscaled_df, original_df, 'LSTM')
+#     # print scores and plot results
+#     get_scores(unscaled_df, original_df, 'LSTM')
+#     plot_results(unscaled_df, original_df, 'LSTM')
 
-def sarimax_model(data):
-    """Runs an arima model with 12 lags and yearly seasonal impact. Generates
-    dynamic predictions for last 12 months. Prints and saves scores and plots
-    results.
-    """
-    # Model
-    sar = sm.tsa.statespace.SARIMAX(data.sales_diff, order=(12, 0, 0),
-                                    seasonal_order=(0, 1, 0, 12),
-                                    trend='c').fit()
+# def sarimax_model(data):
+#     """Runs an arima model with 12 lags and yearly seasonal impact. Generates
+#     dynamic predictions for last 12 months. Prints and saves scores and plots
+#     results.
+#     """
+#     # Model
+#     sar = sm.tsa.statespace.SARIMAX(data.sales_diff, order=(12, 0, 0),
+#                                     seasonal_order=(0, 1, 0, 12),
+#                                     trend='c').fit()
 
-    # Generate predictions
-    start, end, dynamic = 40, 100, 7
-    data['pred_value'] = sar.predict(start=start, end=end, dynamic=dynamic)
+#     # Generate predictions
+#     start, end, dynamic = 40, 100, 7
+#     data['pred_value'] = sar.predict(start=start, end=end, dynamic=dynamic)
 
-    # Generate predictions dataframe
-    original_df = load_data('../data/monthly_data.csv')
-    unscaled_df = predict_df(data, original_df)
+#     # Generate predictions dataframe
+#     original_df = load_data('../data/monthly_data.csv')
+#     unscaled_df = predict_df(data, original_df)
 
-    # print scores and plot results
-    get_scores(unscaled_df, original_df, 'ARIMA')
-    plot_results(unscaled_df, original_df, 'ARIMA')
+#     # print scores and plot results
+#     get_scores(unscaled_df, original_df, 'ARIMA')
+#     plot_results(unscaled_df, original_df, 'ARIMA')
 
-def main():
-    """Calls all functions to load data, run regression models, run lstm model,
-    and run arima model.
-    """
-    # Regression models
-    model_df = load_data('../data/model_df.csv')
-    train, test = tts(model_df)
+# def sarimax_model(data):
+#     """Runs an arima model with 12 lags and yearly seasonal impact. Generates
+#     dynamic predictions for last 12 months. Prints and saves scores and plots
+#     results.
+#     """
+#     # Model
+#     sar = sm.tsa.statespace.SARIMAX(data.sales_diff, order=(12, 0, 0),
+#                                     seasonal_order=(0, 1, 0, 12),
+#                                     trend='c').fit()
 
-    # Sklearn
-    regressive_model(train, test, LinearRegression(), 'LinearRegression')
-    regressive_model(train, test, RandomForestRegressor(n_estimators=100,
-                                                        max_depth=20),
-                     'RandomForest')
-    regressive_model(train, test, XGBRegressor(n_estimators=100,
-                                               learning_rate=0.2,
-                                               objective='reg:squarederror'),
-                     'XGBoost')
-    # Keras
-    lstm_model(train, test)
+#     # Generate predictions
+#     start, end, dynamic = 40, 100, 7
+#     data['pred_value'] = sar.predict(start=start, end=end, dynamic=dynamic)
 
-    # Arima
-    ts_data = load_data('../data/arima_df.csv').set_index('date')
-    ts_data.index = pd.to_datetime(ts_data.index)
+#     # Generate predictions dataframe
+#     original_df = load_data('../data/monthly_data.csv')
+#     unscaled_df = predict_df(data, original_df)
 
-    sarimax_model(ts_data)
+#     # print scores and plot results
+#     get_scores(unscaled_df, original_df, 'ARIMA')
+#     plot_results(unscaled_df, original_df, 'ARIMA')

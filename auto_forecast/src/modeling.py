896200fd5079fa591fd100773a78a1105b8a583a
molly.ruby <molly.ruby@effem.com>
2023-08-22 09:50:17 -0400

    added arima model capability

--- auto_forecast/src/modeling.py
+++ auto_forecast/src/modeling.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import numpy as np
+
+# Import sklearn packages
 from sklearn.metrics import (mean_squared_error, 
                              mean_absolute_error, 
                             r2_score)
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from xgboost.sklearn import XGBRegressor
+
+# import LSTM packages
+import keras
+from keras.layers import Dense, LSTM
+from keras.models import Sequential
+
+from pmdarima.arima import auto_arima
 
 def isvalid_value(value, values_list):
     if value not in values_list:
@@ -29,6 +38,14 @@ class SalesForecasting:
                                         learning_rate=0.2, 
                                         objective='reg:squarederror'),
                 'type': 'regression'
+            },
+            'LSTM': {
+                'model': Sequential(),
+                'type': 'lstm'
+            },
+            'ARIMA': {
+                'model': 'auto_arima',
+                'type': 'arima'
             }
         }
 
@@ -47,16 +64,39 @@ class SalesForecasting:
         model_list_options = self.model_list_options
         self.X_train = X_train
         self.y_train = y_train
+        self.train_index = X_train.index
 
         # train each of the models provided in model_dict 
         for model_name in self.model_list:
-            if model_list_options[model_name]['type']=='regression':
-                self.stored_models[model_name]['model'] = self.__fit_regression_model(model_list_options[model_name]['model'])
+            model_type = model_list_options[model_name]['type']
+            model_option = model_list_options[model_name]['model']
+            if model_type=='regression':
+                self.stored_models[model_name]['model'] = self.__fit_regression_model(model_option)
+            elif model_type=='lstm':
+                self.stored_models[model_name]['model'] = self.__fit_lstm_model(model_option)
+            elif model_type=='arima':
+                self.stored_models[model_name]['model'] = self.__fit_arima_model(model_name) 
             else: 
                 print('Model fit not found.')
     
     def __fit_regression_model(self, model):
         return model.fit(self.X_train, self.y_train)
+    
+    def __fit_lstm_model(self, model):
+        X_train_lstm = self.X_train.values
+        X_train_lstm = X_train_lstm.reshape(X_train_lstm.shape[0], 1, X_train_lstm.shape[1])
+
+        model.add(LSTM(4, batch_input_shape=(1, X_train_lstm.shape[1], X_train_lstm.shape[2]), 
+                   stateful=True))
+        model.add(Dense(1))
+        model.add(Dense(1))
+        model.compile(loss='mean_squared_error', optimizer='adam')
+        return model.fit(X_train_lstm, self.y_train, epochs=200, batch_size=1, verbose=1, shuffle=False)
+    
+    def __fit_arima_model(self, model_name):
+        model = auto_arima(self.y_train, max_p=12, seasonal=True, m=12)
+        self.model_list_options[model_name]['model'] = model
+        return model
     
     def predict(self, x_values, y_values=None, scaler=None, print_scores=False):
 
@@ -64,11 +104,16 @@ class SalesForecasting:
 
         self.y_validation_values = y_values
         self.x_predictor_values = x_values
+        self.test_index = x_values.index
         self.scaler = scaler
 
         for model_name in self.model_list:
             if model_list_options[model_name]['type']=='regression':
                 predictions = self.__predict_regression_model(model_list_options[model_name]['model'])
+            elif model_list_options[model_name]['type']=='lstm':
+                predictions = self.__predict_lstm_model(model_list_options[model_name]['model'])
+            elif model_list_options[model_name]['type']=='arima':
+                predictions = self.__predict_arima_model(model_list_options[model_name]['model'])
             else: 
                 print('Model fit not found.')
 
@@ -89,6 +134,16 @@ class SalesForecasting:
     
     def __predict_regression_model(self, model):
         return model.predict(self.x_predictor_values)
+    
+    def __predict_lstm_model(self, model):
+        X_test_lstm = self.x_predictor_values.values
+        X_test_lstm = X_test_lstm.reshape(X_test_lstm.shape[0], 1, X_test_lstm.shape[1])
+        return model.predict(X_test_lstm, batch_size=1)
+    
+    def __predict_arima_model(self, model):
+        num_predictions = len(self.y_validation_values)
+        return np.array(model.predict(n_periods=num_predictions))
+
 
     def __undo_scaling(self, values, scaler):
         return scaler.inverse(values)
@@ -112,81 +167,41 @@ class SalesForecasting:
 
         return rmse, mae, r2
     
+    def plot_results(self, model_list=None, xlabel="Date", ylabel="Sales", title="Sales Forecasting Predictions"):
+        import parameters as p
+        import seaborn as sns
 
-# def lstm_model(train_data, test_data):
-#     """Runs a long-short-term-memory nueral net with 2 dense layers. Generates
-#     predictions that are then unscaled. Scores are printed and results are
-#     plotted and saved.
+        # concatenate train and test to get all y values
+        y_values = self.y_train
+        test_index = self.test_index
+        train_index = self.train_index
+        y_val_values = self.y_validation_values
+        stored_models = self.stored_models
 
-#     Keyword arguments:
-#     -- train_set: dataset used to train the model
-#     -- test_set: dataset used to test the model
-#     """
+        if y_val_values is not None: 
+            y_values += y_val_values
 
-#     # Split into X & y and scale data
-#     X_train, y_train, X_test, y_test, scaler_object = scale_data(train_data, test_data)
+        total_index = train_index + test_index
 
-#     X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1])
-#     X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1])
+        if model_list == None:
+            model_list == list(stored_models.keys())
 
-#     # Build LSTM
-#     model = Sequential()
-#     model.add(LSTM(4, batch_input_shape=(1, X_train.shape[1], X_train.shape[2]), stateful=True))
-#     model.add(Dense(1))
-#     model.add(Dense(1))
-#     model.compile(loss='mean_squared_error', optimizer='adam')
-#     model.fit(X_train, y_train, epochs=200, batch_size=1, verbose=1, shuffle=False)
-#     predictions = model.predict(X_test, batch_size=1)
+        fig, ax = plt.subplots(figsize=p.FIG_SIZE)
+        sns.lineplot(total_index, y_values, ax=ax, label='Actual', color='blue')
+        
+        for mod in model_list:
+            sns.lineplot(test_index, stored_models[mod]['predictions'], ax=ax, label=f'{mod}_Predictions')
 
-#     # Undo scaling to compare predictions against original data
-#     original_df = load_data('../data/monthly_data.csv')
-#     unscaled = undo_scaling(predictions, X_test, scaler_object, lstm=True)
-#     unscaled_df = predict_df(unscaled, original_df)
+        ax.set(xlabel="Date",
+                ylabel="Sales",
+                title=title)
+        ax.legend()
+        sns.despine()
+        return fig
 
-#     # print scores and plot results
-#     get_scores(unscaled_df, original_df, 'LSTM')
-#     plot_results(unscaled_df, original_df, 'LSTM')
+        
 
-# def sarimax_model(data):
-#     """Runs an arima model with 12 lags and yearly seasonal impact. Generates
-#     dynamic predictions for last 12 months. Prints and saves scores and plots
-#     results.
-#     """
-#     # Model
-#     sar = sm.tsa.statespace.SARIMAX(data.sales_diff, order=(12, 0, 0),
-#                                     seasonal_order=(0, 1, 0, 12),
-#                                     trend='c').fit()
 
-#     # Generate predictions
-#     start, end, dynamic = 40, 100, 7
-#     data['pred_value'] = sar.predict(start=start, end=end, dynamic=dynamic)
+        
 
-#     # Generate predictions dataframe
-#     original_df = load_data('../data/monthly_data.csv')
-#     unscaled_df = predict_df(data, original_df)
-
-#     # print scores and plot results
-#     get_scores(unscaled_df, original_df, 'ARIMA')
-#     plot_results(unscaled_df, original_df, 'ARIMA')
-
-# def sarimax_model(data):
-#     """Runs an arima model with 12 lags and yearly seasonal impact. Generates
-#     dynamic predictions for last 12 months. Prints and saves scores and plots
-#     results.
-#     """
-#     # Model
-#     sar = sm.tsa.statespace.SARIMAX(data.sales_diff, order=(12, 0, 0),
-#                                     seasonal_order=(0, 1, 0, 12),
-#                                     trend='c').fit()
-
-#     # Generate predictions
-#     start, end, dynamic = 40, 100, 7
-#     data['pred_value'] = sar.predict(start=start, end=end, dynamic=dynamic)
-
-#     # Generate predictions dataframe
-#     original_df = load_data('../data/monthly_data.csv')
-#     unscaled_df = predict_df(data, original_df)
-
-#     # print scores and plot results
-#     get_scores(unscaled_df, original_df, 'ARIMA')
-#     plot_results(unscaled_df, original_df, 'ARIMA')
+    
